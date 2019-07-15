55e25706c8e70009dbc609767a1b37bfa44657ef
Masaya Kataoka <ms.kataoka@gmail.com>
2019-07-15 21:26:59 +0900

    fix pose_to_twist launch files

--- include/twist_calculator/pose_to_twist.h
+++ include/twist_calculator/pose_to_twist.h
@@ -5,7 +5,10 @@
 #include <ros/ros.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <quaternion_operation/quaternion_operation.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 //headers in boost
 #include <boost/circular_buffer.hpp>
@@ -20,6 +23,7 @@ namespace twist_calculator
     private:
         bool publish_timestamp_;
         std::string robot_frame_;
+        std::string map_frame_;
         std::string pose_topic_;
         ros::NodeHandle nh_;
         ros::NodeHandle pnh_;
@@ -27,6 +31,8 @@ namespace twist_calculator
         void poseCallback(const geometry_msgs::PoseStamped::ConstPtr msg);
         ros::Subscriber pose_sub_;
         boost::circular_buffer<geometry_msgs::PoseStamped> pose_buffer_;
+        tf2_ros::TransformListener listener_;
+        tf2_ros::Buffer buffer_;
     };
 }
 
--- src/pose_to_twist.cpp
+++ src/pose_to_twist.cpp
@@ -4,11 +4,13 @@
 namespace twist_calculator
 {
     PoseToTwist::PoseToTwist(ros::NodeHandle nh,ros::NodeHandle pnh)
+     : listener_(buffer_)
     {
         nh_ = nh;
         pnh_ = pnh;
         pnh_.param<bool>("publish_timestamp", publish_timestamp_, false);
         pnh_.param<std::string>("robot_frame", robot_frame_, "/base_link");
+        pnh_.param<std::string>("map_frame", map_frame_, "/map");
         pnh_.param<std::string>("pose_topic", pose_topic_, "/robot_pose");
         pose_buffer_ = boost::circular_buffer<geometry_msgs::PoseStamped>(2);
         if(publish_timestamp_)
@@ -29,7 +31,27 @@ namespace twist_calculator
 
     void PoseToTwist::poseCallback(const geometry_msgs::PoseStamped::ConstPtr msg)
     {
-        pose_buffer_.push_back(*msg);
+        if(msg->header.frame_id != map_frame_)
+        {
+            geometry_msgs::TransformStamped transform_stamped;
+            try
+            {
+                transform_stamped = buffer_.lookupTransform(map_frame_, msg->header.frame_id, msg->header.stamp);
+            }
+            catch (tf2::TransformException &ex)
+            {
+                ROS_WARN("%s",ex.what());
+                ros::Duration(1.0).sleep();
+                return;
+            }
+            geometry_msgs::PoseStamped transformed_pose;
+            tf2::doTransform(*msg,transformed_pose,transform_stamped);
+            pose_buffer_.push_back(transformed_pose);
+        }
+        else
+        {
+            pose_buffer_.push_back(*msg);
+        }
         using namespace quaternion_operation;
         if(pose_buffer_.size() != 2)
         {
@@ -47,19 +69,6 @@ namespace twist_calculator
         twist.twist.angular.y = twist.twist.angular.y/dt;
         twist.twist.angular.z = twist.twist.angular.z/dt;
         Eigen::Matrix3d rotation_mat = getRotationMatrix(conjugate(pose_buffer_[0].pose.orientation));
-        Eigen::Vector3d vec0,vec1;
-        vec0[0] = pose_buffer_[0].pose.position.x;
-        vec0[1] = pose_buffer_[0].pose.position.y;
-        vec0[2] = pose_buffer_[0].pose.position.z;
-        vec1[0] = pose_buffer_[1].pose.position.x;
-        vec1[1] = pose_buffer_[1].pose.position.y;
-        vec1[2] = pose_buffer_[1].pose.position.z;
-        vec0 = rotation_mat*vec0;
-        vec1 = rotation_mat*vec1;
-        twist.twist.linear.x = (vec1[0]-vec0[0])/dt;
-        twist.twist.linear.y = (vec1[1]-vec0[1])/dt;
-        twist.twist.linear.z = (vec1[2]-vec0[2])/dt;
-        /*
         Eigen::Vector3d trans_vec;
         trans_vec[0] = pose_buffer_[1].pose.position.x-pose_buffer_[0].pose.position.x;
         trans_vec[1] = pose_buffer_[1].pose.position.y-pose_buffer_[0].pose.position.y;
@@ -68,7 +77,6 @@ namespace twist_calculator
         twist.twist.linear.x = trans_vec[0]/dt;
         twist.twist.linear.y = trans_vec[1]/dt;
         twist.twist.linear.z = trans_vec[2]/dt;
-        */
         if(publish_timestamp_)
         {
             twist_pub_.publish(twist);
